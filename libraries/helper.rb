19ee8ff39e1aad56cbb86fd834baebca660f6629
Thiru Moorthy <thirumoorthi.r@outlook.com>
2020-11-07 22:07:08 +0530

    error fixing 1

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -3,7 +3,10 @@ module EBT
     include Chef::Mixin::PowershellOut
 
     def remote_desktop_session_host?
-      win_version = Chef::ReservedNames::Win32::Version.new
+      before do
+        win_version = allow(Chef::ReservedNames::Win32::Version).to receive(:new).and_return(double('fake version', windows_server_2008?: false))
+      end
+      # win_version = Chef::ReservedNames::Win32::Version.new
 
       if win_version.windows_server_2008_r2? || win_version.windows_server_2012? || win_version.windows_server_2012_r2? || win_version.windows_server_2016? || win_version.windows_server_2019?
         cmd = powershell_out!('Get-WindowsFeature RDS-RD-Server', returns: [0, 2])
