22a040faccc07d70c15b1d7eebe74870625a6c9f
Thiru Moorthy <thirumoorthi.r@outlook.com>
2020-11-07 22:10:25 +0530

    added require

--- libraries/helper.rb
+++ libraries/helper.rb
@@ -1,12 +1,11 @@
+require 'chef/win32/version'
+
 module EBT
   module Helpers
     include Chef::Mixin::PowershellOut
 
     def remote_desktop_session_host?
-      before do
-        win_version = allow(Chef::ReservedNames::Win32::Version).to receive(:new).and_return(double('fake version', windows_server_2008?: false))
-      end
-      # win_version = Chef::ReservedNames::Win32::Version.new
+      win_version = Chef::ReservedNames::Win32::Version.new
 
       if win_version.windows_server_2008_r2? || win_version.windows_server_2012? || win_version.windows_server_2012_r2? || win_version.windows_server_2016? || win_version.windows_server_2019?
         cmd = powershell_out!('Get-WindowsFeature RDS-RD-Server', returns: [0, 2])
